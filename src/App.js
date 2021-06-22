a5c6aebc650461737e979e241cf924d28f304362
Bene-Hur Pessoa <benehurp@gmail.com>
2021-06-22 17:30:23 -0300

    Header criado em HTML e CSS de forma responsiva.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div>Tudo pronto!</div>;
+  return (
+    <>
+      <header>
+        <div class="loginBar">
+          <b>Acesse sua Conta</b> ou <b>Cadastre-se</b>
+        </div>
+        <div class="headerContainer">
+          <div class="logo">
+            <img
+              src="./assets/logo_header.jpg"
+              alt="Logo: Webump! More than just a plataform"
+            />
+          </div>
+          <div class="searchBar">
+            <input type="text" />
+            <button>BUSCAR</button>
+          </div>
+        </div>
+        <menu>
+          <div class="menuContainer">
+            <ul>
+              <li>Página Inicial</li>
+              <li>Camisetas</li>
+              <li>Calças</li>
+              <li>Sapatos</li>
+              <li>Contato</li>
+            </ul>
+          </div>
+        </menu>
+      </header>
+    </>
+  );
 }
 
 export default App;
